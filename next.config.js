42ce31292c99f2e9c0e183e01b7d0f40389aeca7
skaunov <skaunov@disroot.org>
2023-06-19 17:02:12 -0400

    fix stream

--- next.config.js
+++ next.config.js
@@ -1,18 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config, { isServer }) => {
-        // Add the polyfill to the client-side webpack config
-        if (!isServer) {
-          config.resolve.fallback = {
-            ...config.resolve.fallback,
-            "stream": require.resolve("stream-browserify"),
-            "stream/web": require.resolve("stream-browserify"),
-            "stream-transform": require.resolve("web-streams-polyfill/dist/ponyfill.es2018.js"),
-          };
-        }
-    
-        return config;
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        "stream": require.resolve("stream-browserify"),
+        "stream/web": require.resolve("stream-browserify"),
+        "stream-transform": require.resolve("web-streams-polyfill/dist/ponyfill.es2018.js"),
+        "web-streams-polyfill": require.resolve("web-streams-polyfill")
+      };
     }
-}
 
-module.exports = nextConfig
+    return config;
+  }
+};
+
+module.exports = nextConfig;
